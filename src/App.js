b5610b7a02b481378a38204938a090931cd8e655
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-02-11 20:20:04 +0530

    Prepared the project skeleton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+// import the styles
+import './styles/style.css';
+
+import Header from './components/Header';
+import Meme from './components/Meme'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Meme />
+    </>
   );
 }
 
